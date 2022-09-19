137e7e6f705a89477f7bbd47c4c6e1634e84d5a4
AkhileshSoorneedi <48817190+AkhileshSoorneedi@users.noreply.github.com>
2022-09-19 14:18:52 +1000

    project

---
+++ Data/Migrations/20220919030206_initialsetup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    firstname = table.Column<string>(nullable: false),
+                    lastName = table.Column<string>(nullable: false),
+                    email = table.Column<string>(nullable: false),
+                    mobileNumber = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Users
+    {
+        public int id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string firstname { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string email { get; set; }
+
+        [Display(Name = "Mobile Number")]
+        [DataType(DataType.PhoneNumber)]
+        public int mobileNumber { get; set; }
+
+        public Users()
+        {
+
+        }
+    }
+}
